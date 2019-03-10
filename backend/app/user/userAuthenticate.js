48003e2a8f37e2737f87c4c5d85d9fa8d2762a48
Tae yoon <15tyc@queensu.ca>
2019-03-10 09:01:20 -0400

    Testing Tokens

--- backend/app/user/userAuthenticate.js
+++ backend/app/user/userAuthenticate.js
@@ -10,23 +10,29 @@ router.post('/', (req,res) => {
     const { email, password } = req.body;
     User.findOne({email}, (err,user) => {
         if(err) {
+            console.log("Internal Error");
             res.status(500)
                 .json({ error: 'Internal error please try again' });
         }
         else if (!user){
-            
+            console.log("User Not found");
+            res.status(401)
+                .json({ error: 'Incorrect email or password'})
         }
         else {
             user.isCorrectPassword(password, (err,same) => {
                 if(err){
+                    console.log("error decrypting password")
                     res.status(500)
                         .json({ error: 'Internal error please try again' });
                 }
                 else if(!same){
+                    console.log("password Incorrect");
                     res.status(401)
                         .json({ error:'Incorrect email or password' });
                 }
                 else {
+                    console.log("Sending Token");
                     const payload = { email };
                     const token = jwt.sign(payload, secret, {
                         expiresIn: '1h'
